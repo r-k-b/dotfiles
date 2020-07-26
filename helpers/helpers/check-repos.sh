b07c7fc2f6f0201b7e1a9c11a6222c3603ba21f8
Robert K. Bell <robert.k.bell@gmail.com>
2020-07-26 13:53:15 +1000

    feat(shell): add a helper for git backups
    Problem: I'm prone to forgetting to commit and/or push changes to the
    various repos I'm using around the place. Also prone to forgetting
    the repo exists at all (have I missed any??)
    
    Solution: create a helper to do the checking for me
    
    Note: automate the checker ... should it run on login?

---
+++ helpers/helpers/check-repos.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# Have I forgotten to commit or push changes to the common repos?
+
+set -e
+
+check () {
+	msg=" Checking ${1} ..."
+	msgLen=${#msg}
+	echo 
+	for ((i=1; i<=((80-msgLen)); i++)); do
+		printf '=%.0s' 1
+	done;
+	echo "$msg"
+	git -C "$1" status
+}
+
+check /etc/nixos
+check ~/dotfiles
+
