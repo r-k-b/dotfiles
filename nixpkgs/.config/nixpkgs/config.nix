0c370aa28081e2d523893a3cb0a26298f964708a
Robert K. Bell <robert.k.bell@gmail.com>
2022-05-05 10:51:11 +1000

    allow an insecure package for the CAT tool to use

--- nixpkgs/.config/nixpkgs/config.nix
+++ nixpkgs/.config/nixpkgs/config.nix
@@ -1,2 +1,10 @@
-{ allowUnfree = true; }
+{ allowUnfree = true;
+
+#              # for the CAT tool thing
+#              # https://trello.com/c/xVmIa1fx/4896-cat-change-up-the-group-selector-sidebar-thing
+#              permittedInsecurePackages = [
+#                "nodejs-10.24.1"
+#              ];
+            }
+
 
