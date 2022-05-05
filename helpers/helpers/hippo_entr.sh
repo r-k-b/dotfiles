eaa333a2d134750f6ff5ac98a0a29b5dbb14a40d
Robert K. Bell <robert.k.bell@gmail.com>
2022-05-05 10:50:24 +1000

    work with hippo's new Flake

--- helpers/helpers/hippo_entr.sh
+++ helpers/helpers/hippo_entr.sh
@@ -4,5 +4,5 @@ set -eou pipefail
 
 cd ~/projects/PHDSys-webapp/hippo
 
-nix-shell ./shell.nix --run "ag -l '^module ' ../ | grep .elm | entr sh -c 'clear; elm make src/Main.elm --output=./tmp/elm.js'"
+nix develop ./. --command sh -c "ag -l '^module ' ../ | grep .elm | entr sh -c 'clear; elm make src/Main.elm --output=./tmp/elm.js'"
 
