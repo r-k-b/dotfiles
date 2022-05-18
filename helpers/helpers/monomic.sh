ee1c18743dab16e65a7f36981a737d3b013cc018
Robert K. Bell <robert.k.bell@gmail.com>
2022-05-18 13:06:13 +1000

    feat: fail fast when mic name is wrongwq

--- helpers/helpers/monomic.sh
+++ helpers/helpers/monomic.sh
@@ -7,13 +7,21 @@ if pacmd list-sources | grep -q "Remapped Q1U"; then
 	exit 0;
 fi
 
-pacmd load-module module-remap-source \
-	source_name=cru1_mono \
-	master=alsa_input.usb-USB_Microphone_USB_Microphone-00.analog-stereo \
-	channels=2 \
-	channel_map=mono,mono
+BASE_NAME="alsa_input.usb-USB_Microphone_USB_Microphone-00.iec958-stereo";
 
-pacmd set-default-source cru1_mono
+if pacmd list-sources | grep -q "name: <$BASE_NAME>"; then
+	pacmd load-module module-remap-source \
+		source_name=cru1_mono \
+		master=$BASE_NAME \
+		channels=2 \
+		channel_map=mono,mono
 
-echo "Remapped microphone created."
+	pacmd set-default-source cru1_mono
+
+	echo "Remapped microphone created."
+	exit 0;
+else
+	echo "Base mic not found: $BASE_NAME"
+	exit 1;
+fi;
 
