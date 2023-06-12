27ab7a665397b81b9f915bbfe103e220de40c2c8
Robert K. Bell <robert.k.bell@gmail.com>
2023-06-12 10:07:23 +1000

    fix: pin to a known-working version of the az-cli

--- helpers/helpers/az-cli-docker-alias.sh
+++ helpers/helpers/az-cli-docker-alias.sh
@@ -5,6 +5,6 @@ docker run -it \
     -v "${HOME}"/.azure-devops:/root/.azure-devops \
     -v "${HOME}"/Downloads/azclistuff:/Downloads \
     -v ~/projects/PHDSys-webapp/hippo:/hippo \
-    mcr.microsoft.com/azure-cli:latest \
+    mcr.microsoft.com/azure-cli:2.40.0 \
     az "$@"
 
