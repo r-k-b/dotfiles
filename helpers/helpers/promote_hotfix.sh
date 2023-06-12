fb867871f6957cb7521322de9e7071040a5f6d5f
Robert K. Bell <robert.k.bell@gmail.com>
2023-06-12 10:39:27 +1000

    feat: apply the same changes as the last few commits to `promote.sh`

--- helpers/helpers/promote_hotfix.sh
+++ helpers/helpers/promote_hotfix.sh
@@ -1,10 +1,21 @@
 #!/usr/bin/env bash
 
-# use like: ./promote_hotfix.sh "hippoui-release.hippo.2023w12.1" "2023W12.1" e7750d6e065dcd8ab314846a0530556bdf423dd3
-# (the first arg can be copied from the name of the feed in Azure Artifacts;
-# it's derived from the name of the hotfix branch)
-
 set -eou pipefail
+
+hash az 2>/dev/null || {
+  echo >&2 "I didn't find the 'az' command.  Do you have the azure-cli set up?";
+  exit 1;
+}
+
+if [ $# -eq 0 ]
+then
+	# no arguments provided
+	echo 'use like: ./promote.sh "hippoui-release.hippo.2023w12.1" "2023W12.1" e7750d6e065dcd8ab314846a0530556bdf423dd3'
+	echo ''
+	echo 'Note: the first arg exactly matches the "name" property of the feed in Azure Artifacts.'
+  echo "It's derived from the name of the hotfix branch."
+	exit 0
+fi
 
 workDir=$(realpath ~/Downloads/azclistuff)
 echo "Working in $workDir"
@@ -16,21 +27,21 @@ hotfixFeed="$1"
 
 echo downloading commit hash: "$commitHash"
 
-~/helpers/az-cli-docker-alias.sh artifacts universal download \
+az artifacts universal download \
 	--organization "https://pacifichealthdynamics.visualstudio.com/" \
 	--project "e216e3e9-03bd-4591-b373-c229ed35d45d" \
 	--scope project \
 	--feed "phd-frontend" \
 	--name "$hotfixFeed" \
 	--version "0.0.0-commit-$commitHash"\
-	--path /Downloads/
+	--path /tmp/
 
 
 sanitizedReleaseVersion=$(printf '%s' "$releaseVersion" | tr '[:upper:]' '[:lower:]')
 
 echo uploading to the main feed for WF Octo, as version: "$sanitizedReleaseVersion ($releaseVersion)"
 
-~/helpers/az-cli-docker-alias.sh artifacts universal publish \
+az artifacts universal publish \
 	--organization https://pacifichealthdynamics.visualstudio.com/ \
 	--project 'PHDSys' \
 	--scope 'project' \
@@ -38,7 +49,7 @@ echo uploading to the main feed for WF Octo, as version: "$sanitizedReleaseVersi
 	--name "hippoui-main" \
 	--version "0.0.0-commit-$commitHash" \
 	--description "HIPPO frontend for release $releaseVersion, commit $commitHash" \
-	--path "/Downloads/HippoUI.0.0.0-commit-$commitHash.zip"
+	--path "/tmp/HippoUI.0.0.0-commit-$commitHash.zip"
 
 echo renaming zip file
 
@@ -47,7 +58,7 @@ mv "HippoUI.0.0.0-commit-$commitHash.zip" \
 
 echo uploading as version: "$sanitizedReleaseVersion ($releaseVersion)"
 
-~/helpers/az-cli-docker-alias.sh artifacts universal publish \
+az artifacts universal publish \
 	--organization https://pacifichealthdynamics.visualstudio.com/ \
 	--project 'PHDSys' \
 	--scope 'project' \
@@ -55,6 +66,6 @@ echo uploading as version: "$sanitizedReleaseVersion ($releaseVersion)"
 	--name "hippoui-release" \
 	--version "0.0.0-release-$sanitizedReleaseVersion" \
 	--description "HIPPO frontend for release $releaseVersion, commit $commitHash" \
-	--path "/Downloads/HippoUI.0.0.0-release-$sanitizedReleaseVersion.zip"
+	--path "/tmp/HippoUI.0.0.0-release-$sanitizedReleaseVersion.zip"
 
 echo done.
