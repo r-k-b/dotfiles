1a02ef7c4b545108b06815b9b73d47aec4b73667
Robert K. Bell <robert.k.bell@gmail.com>
2024-01-22 13:53:33 +1100

    bump to latest version, use as non-root

--- helpers/helpers/az-cli-docker-alias.sh
+++ helpers/helpers/az-cli-docker-alias.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 
-docker run -it \
+# for available versions, see:
+# https://mcr.microsoft.com/v2/azure-cli/tags/list
+
+podman run -it \
     -v "${HOME}"/.azure:/root/.azure \
     -v "${HOME}"/.azure-devops:/root/.azure-devops \
     -v "${HOME}"/Downloads/azclistuff:/Downloads \
     -v ~/projects/PHDSys-webapp/hippo:/hippo \
-    mcr.microsoft.com/azure-cli:2.53.0 \
+    mcr.microsoft.com/azure-cli:2.56.0 \
     az "$@"
 
